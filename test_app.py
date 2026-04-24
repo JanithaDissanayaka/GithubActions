97f33d1f1b70d13baadd2c09b317ac96e78b8435
JanithaDissanayaka <janithadissnayakaa@gmail.com>
2026-04-24 10:24:25 +0530

    test the application

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b'Hello, DevOps World! v1' in response.data
+    assert response.data == b'Hello, DevOps World! v2' in response.data
